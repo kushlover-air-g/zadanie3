b47350a257fef723ff8f5dd811a1b7bdfa3085c6
kushlover-air-g <59054712+kushlover-air-g@users.noreply.github.com>
2019-12-19 20:08:17 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double symm=1;
+            for(int i = 2; i<(Math.PI*Math.PI)/6; i++)
+            {
+                symm += 1 / Math.Pow(i, 2);
+            }
+            Console.WriteLine(symm);
+        }
+    }
+}
